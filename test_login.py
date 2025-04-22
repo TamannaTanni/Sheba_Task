5564b9832fe3ac641417d5bb01dad4fedf0d5de7
Tanni <tamannatanni91@gmail.com>
2025-04-23 00:15:56 +0600

    log added

--- test_login.py
+++ test_login.py
@@ -12,3 +12,5 @@ def test_login(browser_context_with_trace)-> None:
     page.get_by_label("Password", exact=True).fill("admin123")
     page.get_by_role("button", name="Login").click()
     assert "Login Successful!" in page.get_by_role("status").inner_text()
+
+    print(" test")
